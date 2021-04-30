fe29f095743a400106cafa691f4fd5f878fd1385
Prathap <436prathap@gmail.com>
2021-04-30 08:34:34 +0530

    add media queries

--- src/Control.js
+++ src/Control.js
@@ -6,7 +6,7 @@ function Control(props){
         <div className= "Control">
             <div className= "Push">
                 <button onClick= {push} >Push</button>
-                <input placeholder= "Push a value..." maxLength= "5"/>
+                <input placeholder= "Push a value..." maxLength= "5" autofocus= "true"/>
             
             </div>
             <div className= "Pop">
@@ -19,10 +19,14 @@ function Control(props){
         var stackHeight= document.querySelectorAll(".Node").length;
         
         if(stackHeight === 7)
-        alert("Stack is full...");            
-        else
-        props.push(); 
-        document.querySelector(".Push input").value= "";
+            alert("Stack is full...");            
+        
+        else{
+            props.push(); 
+            var input= document.querySelector(".Push input");
+            input.value= "";
+            input.focus();
+        }
     }
     
     function pop(){
